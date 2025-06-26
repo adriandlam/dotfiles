598b62267ee57b945ce68184230add9a8fd1fce0
Adrian Lam <adrianlamdev@gmail.com>
2025-06-25 19:51:08 -0700

    add transparent bg to nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
   config = function()
     local monokai = require("monokai-pro")
     monokai.setup({
+      transparent_background = true,
       devicons = true,
       filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
       inc_search = "background", -- underline | background
