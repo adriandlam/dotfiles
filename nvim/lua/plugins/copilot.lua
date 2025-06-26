595c94ffedf86a4dd34c5f51ee8a3ea59200e63a
Adrian Lam <adrianlamdev@gmail.com>
2025-06-26 15:16:56 -0700

    rename files and remove avante

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      filetypes = {
+        ["*"] = true,
+      },
+    })
+  end,
+}
