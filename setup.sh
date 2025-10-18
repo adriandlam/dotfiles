d929ac9999b0a83c139208fde9d37eb523e1bb97
Adrian Lam <me@adriandlam.com>
2025-10-18 15:54:03 -0700

    update setup.sh for brew and custom zsh stuff

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Install Stow
+brew update
+brew upgrade
+
 brew install stow
+brew install lazygit
+brew install zoxide
+brew install gh
+brew install ghostty
 
 # Create symlinks
 # Run stow from the dotfiles directory, targeting home directory
 stow --target="$HOME" .
+
+# Setup zsh plugins
+brew install zsh-autocomplete
+brew install zsh-autosuggestions
+
 
