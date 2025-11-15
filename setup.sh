17ffb9af5e21354ae05e6cd032592afea717a853
Adrian Lam <me@adriandlam.com>
2025-11-15 10:09:21 -0800

    setup

--- setup.sh
+++ setup.sh
@@ -14,7 +14,11 @@ brew install ghostty
 stow --target="$HOME" .
 
 # Setup zsh plugins
+brew install pure
 brew install zsh-autocomplete
 brew install zsh-autosuggestions
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
